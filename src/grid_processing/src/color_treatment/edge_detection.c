2f3e9d135bab83ab775c42a4e555d2f00d500bc6
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-12-01 18:35:12 +0100

    clean all treatment funtion

--- src/grid_processing/src/color_treatment/edge_detection.c
+++ src/grid_processing/src/color_treatment/edge_detection.c
@@ -2,84 +2,78 @@
 
 int get_new_value(SDL_Surface *img,int i, int j)
 {
-     /*
-       return if point(i,j) is black or white
-      */
+    /*
+      return if point(i,j) is black or white
+    */
     int w = img->w;
     int h = img -> h;
     if(pixel_value( img, i, j) != 0)
-	  return 1;
-     for(int di = -1;di<2;di+=2)
-	  for(int dj = -1;dj<2;dj+=2)
-              if ( i+di>0 && i+di<w && j+dj>0 && j+dj<h && pixel_value( img, i+di, j+dj) != 0)
-		    return 0;
-     return 1;
+        return 1;
+    for(int di = -1;di<2;di+=2)
+        for(int dj = -1;dj<2;dj+=2)
+            if ( i+di>0 && i+di<w && j+dj>0 && j+dj<h && pixel_value( img, i+di, j+dj) != 0)
+                return 0;
+    return 1;
 }
 
 double convolution(SDL_Surface *img , double matr[3*3], int x, int y)
 {
-     /*
-       return result on convolution of matr in point(x,y) from img
-      */
-     double sum = 0;
-     int index = 0;
+    /*
+      return result on convolution of matr in point(x,y) from img
+    */
+    double sum = 0;
+    int index = 0;
 
-     Uint32 pixel;
-     Uint8 r,g,b;
-
-    for(int i = -1;i<=1;i++)
+    for (int i = -1; i <= 1; i++)
     {
-	 for(int j = -1;j<=1;j++)
-	 {
-	      if(x+i<img->w && x+i >= 0 && y+j<img->h && y+j >= 0)
-	      {
-		   pixel = get_pixel(img,i+x,j+y);
-		   SDL_GetRGB(pixel, img->format, &r, &g, &b);
-		   sum += r*matr[index];
-	      }
-	      index += 1;
-	 }
+        for (int j = -1; j <= 1; j++)
+        {
+            if (x+i < img->w && x+i >= 0 && y+j < img->h && y+j >= 0)
+                sum += pixel_value(img, i + x, j + y) * matr[index];
+            index += 1;
+        }
     }
     return sum;
 }
 
 SDL_Surface *Sobel(SDL_Surface *img, int var)
 {
-     /*
-       apply Sobel filter and inverse result
-      */
-     Uint32 newpixel;
-     int w = img->w;
-     int h = img -> h;
+    /*
+      apply Sobel filter and inverse result
+    */
+    int w = img->w;
+    int h = img -> h;
+    int color;
+    double gx,gy,val;
 
-     double gx,gy,val;
+    SDL_Surface *result = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
 
-     SDL_Surface *result = SDL_CreateRGBSurface(0,w,h,32,0,0,0,0);
+    double kernelx[3*3] =
+        {-1.0, 0.0, 1.0,
+         -2.0, 0.0, 2.0,
+         -1.0, 0.0, 1.0};
 
-     double kernelx[3*3] = {-1.0, 0.0, 1.0,
-	  -2.0, 0.0, 2.0,
-	  -1.0, 0.0, 1.0};
+    double kernely[3*3] =
+        {-1.0,-2.0,-1.0,
+         0.0,0.0,0.0,
+         1.0,2.0,1.0};
 
-     double kernely[3*3] = {-1.0,-2.0,-1.0,
-	  0.0,0.0,0.0,
-	  1.0,2.0,1.0};
+    for (int i = 2; i < w - 2; i++)
+    {
+        for (int j = 2; j < h - 2; j++)
+        {
+            gx = convolution(img, kernelx, i, j);
+            gy = convolution(img, kernely, i, j);
 
-     for(int i = 2;i<w-2;i++)
-     {
-	  for(int j = 2;j<h-2;j++)
-	  {
-	       gx = convolution(img, kernelx, i,j);
-	       gy = convolution(img, kernely, i,j);
-
-	       val = sqrt(gx*gx + gy*gy);
-	       if(val <= 128)
-		    newpixel = SDL_MapRGB(img->format, 0, 0, 0);
-	       else
-		    newpixel = SDL_MapRGB(img->format, 255, 255, 255);
-	       put_pixel(result, i,j,newpixel);
-	  }
-     }
-     if (var)
-         SDL_FreeSurface(img);
-     return result;
+            val = sqrt(gx * gx + gy * gy);
+            if(val <= 128)
+                color = 0;
+            else
+                color = 255;
+            put_pixel_value(result, i, j, color);
+        }
+    }
+    if (var)
+        SDL_FreeSurface(img);
+    return result;
 }
--- src/grid_processing/src/color_treatment/noise.c
+++ src/grid_processing/src/color_treatment/noise.c
@@ -2,63 +2,57 @@
 
 void sort(int* tab,int len)
 {
-     /*
-       quick sort
-     */
-     if(len < 2)
-	  return;
+    /*
+      quick sort
+    */
+    if(len < 2)
+        return;
 
-     int pivot = tab[len / 2];
+    int pivot = tab[len / 2];
 
-     int i, j;
-     for (i = 0, j = len - 1; ; i++, j--)
-     {
-	  while(tab[i] < pivot)
-	       i++;
-	  while(tab[j] > pivot)
-	       j--;
+    int i, j;
+    for (i = 0, j = len - 1; ; i++, j--)
+    {
+        while(tab[i] < pivot)
+            i++;
+        while(tab[j] > pivot)
+            j--;
 
-	  if(i >= j)
-	       break;
+        if(i >= j)
+            break;
 
-	  int temp = tab[i];
-	  tab[i] = tab[j];
-	  tab[j] = temp;
-     }
+        int temp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = temp;
+    }
 
-  sort(tab, i);
-  sort(tab + i, len - i);
+    sort(tab, i);
+    sort(tab + i, len - i);
 }
 
 SDL_Surface *reduce_noise(SDL_Surface *img)
 {
-     /*
-       apply filter to reduce noise of img
-      */
-    SDL_Surface *oi = SDL_CreateRGBSurface(0,img->w,img->h,32,0,0,0,0);
-    Uint8 r,g,b;
-    Uint32 pixel;
+    /*
+      apply filter to reduce noise of img
+    */
+    SDL_Surface *oi = SDL_CreateRGBSurface(0, img->w, img->h, 32, 0, 0, 0, 0);
     int *matr = malloc(9*sizeof(int));
     int index = 0;
 
-    for(int ix=0; ix < img->w; ix++) {
-	for(int iy=0; iy < img->h; iy++) {
-	     index = 0;
-	     for(int kx=-1; kx <= 1; kx++) {
-		  for(int ky=-1; ky <= 1; ky++) {
-		       if(ix+kx>=0 && ix+kx<img->w && iy+ky>=0 && iy+ky<img->h){
-			    pixel = get_pixel(img, ix+kx,iy+ky);
-			    SDL_GetRGB(pixel, img->format, &r, &g, &b);
-			    matr[index] = r;
-		       }
-		       else
-			    matr[index] = 0;
-		       index += 1;
-		  }
-	     }
-	     sort(matr,9);
-	     pixel = SDL_MapRGB(img->format, matr[4], matr[4], matr[4]);
-	     put_pixel(oi, ix, iy, pixel);
+    for (int i = 0; i < img->w; i++) {
+	for (int j = 0; j < img->h; j++) {
+            index = 0;
+            for(int ki=-1; ki <= 1; ki++) {
+                for(int kj=-1; kj <= 1; kj++) {
+                    if (i+ki >= 0 && i+ki < img->w && j+kj >= 0 && j+kj < img->h)
+                        matr[index] = pixel_value(img, i + ki, j + kj);
+                    else
+                        matr[index] = 0;
+                    index += 1;
+                }
+            }
+            sort(matr,9);
+            put_pixel_value(oi, i, j, matr[4]);
 	}
     }
     free(matr);
@@ -68,34 +62,30 @@ SDL_Surface *reduce_noise(SDL_Surface *img)
 
 int noise(SDL_Surface* img, int w, int h)
 {
-     /*
-       return noise level on image done
-     */
-     double sum = 0;
-     Uint8 r, g, b;
-     int count = 0;
-     double val = 0;
-     Uint32 pixel;
-     for(int i = 0; i<w;i++)
-	  for(int j = 0; j<h; j++)
-	  {
-	       sum=0;
-	       for(int kx=-1; kx <= 1; kx++)
-		    for(int ky=-1; ky <= 1; ky++)
-			 if(i+kx>=0 && i+kx<img->w && j+ky>=0 && j+ky<img->h)
-			 {
-			      pixel = get_pixel(img, i+kx, j+ky);
-			      SDL_GetRGB(pixel, img->format, &r, &g, &b);
-			      sum += r;
-			 }
-	       pixel = get_pixel(img, i, j);
-	       SDL_GetRGB(pixel, img->format, &r, &g, &b);
-	       sum = sum/9.0;
-	       val = 1-((double)r/sum);
-	       if(val < 0)
-		    val *= -1;
-	       if(val > 0.5)
-		    count += 1;
-	  }
-     return count;
+    /*
+      return noise level on image done
+    */
+    double sum = 0;
+    int count = 0;
+    double val = 0;
+
+    for (int i = 0; i<w; i++)
+    {
+        for (int j = 0; j<h; j++)
+        {
+            sum=0;
+            for(int ki = -1; ki <= 1; ki++)
+                for(int kj = -1; kj <= 1; kj++)
+                    if(i+ki >= 0 && i+ki < img->w && j+kj >= 0 && j+kj < img->h)
+                        sum += pixel_value(img, i + ki, j + kj);
+            sum = sum/9.0;
+            double color = pixel_value(img, i, j);
+            val = 1 - color / sum;
+            if(val < 0)
+                val *= -1;
+            if(val > 0.5)
+                count += 1;
+        }
+    }
+    return count;
 }
