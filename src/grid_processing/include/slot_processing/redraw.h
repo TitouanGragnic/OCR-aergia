61ea51029f43bd517a2830f914c9a3845de73191
titouan.guesdon <titouan.guesdon@epita.fr>
2022-12-03 20:02:48 +0100

    matrix to number

---
+++ src/grid_processing/include/slot_processing/redraw.h
@@ -0,0 +1,15 @@
+#ifndef REDRAW_H_
+#define REDRAW_H_
+
+#include <err.h>
+#include <stdlib.h>
+#include <math.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "../utils/pixel_operations.h"
+#include "../utils/init.h"
+#include "../detection/grid_detection.h"
+
+void matrixToSurface(int *matrix, SDL_Surface *img, int x, int y, int u, int v);
+
+#endif
