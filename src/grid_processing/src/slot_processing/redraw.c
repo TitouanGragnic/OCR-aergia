e9c805738a80172e86c61df04d30f1bfdda0d9d3
Titouan Guesdon <titouan.guesdon@epita.fr>
2022-12-04 14:22:00 +0100

    fix redraw for number

--- src/grid_processing/src/slot_processing/redraw.c
+++ src/grid_processing/src/slot_processing/redraw.c
@@ -2,25 +2,24 @@
 
 void matrixToSurface(int *matrix, SDL_Surface *img, int x, int y, int u, int v)
 {
-     int neww = u-x;
-     int newh = v-y;
+    int neww = u-x;
+    int newh = v-y;
 
-     int divW = neww/16;
-     int divH = newh/16;
+    int divW = neww/16;
+    int divH = newh/16;
 
-     Uint32 newPixel = SDL_MapRGB(img->format, 0, 0, 0);
-     Uint32 white = SDL_MapRGB(img->format, 255, 255, 255);
+    Uint32 newPixel = SDL_MapRGB(img->format, 0, 0, 0);
+    Uint32 white = SDL_MapRGB(img->format, 255, 0, 0);
 
-     for (int i = x;i<u; i++)
-     {
-	  for (int j = y; j<v; j++)
-	  {
-	       if (matrix[i/divW*16+j/divH] == 1)
-		    put_pixel(img,i,j,newPixel);
-	       else
-		    put_pixel(img, i, j, white);
-		    
-	  }
-     }
-     free(matrix);
+    for (int i = x;i<u; i++)
+    {
+        for (int j = y; j<v; j++)
+        {
+            if (matrix[(i-u)/divW*16+(j-v)/divH] == 1)
+                put_pixel(img,i,j,newPixel);
+            else
+                put_pixel(img, i, j, white);
+        }
+    }
+    free(matrix);
 }
