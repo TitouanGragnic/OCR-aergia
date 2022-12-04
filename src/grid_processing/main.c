3366dbcbe70deb06ea16f0b1ca5cf09c3d7abb4f
Titouan Guesdon <titouan.guesdon@epita.fr>
2022-12-04 15:05:01 +0100

    main multiThreading implementation

--- src/grid_processing/main.c
+++ src/grid_processing/main.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
     size_t NUM_THREADS = 12*2;
     size_t th = 0;
     pthread_t threads[NUM_THREADS];
-
+    struct img_file out[NUM_THREADS];
     SDL_Surface* edge_surface;
     SDL_Surface* bin_surface;
     SDL_Surface* screen_surface;
@@ -70,19 +70,18 @@ int main(int argc, char *argv[])
 
     // ----------------------Grayscale-----------------------------------------
     grayscale(edge_surface);
-    struct img_file out;
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/grayscale.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/grayscale.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
     if(dev_mod)
         screen_surface = display_image(edge_surface);
 
     // ----------------------Increase_Contrast---------------------------------
     max = increase_contrast(edge_surface,10);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/contrast.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/contrast.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -90,9 +89,9 @@ int main(int argc, char *argv[])
 
     // ----------------------Brightness----------------------------------------
     brightness(edge_surface,max);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/brightness.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/brightness.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -100,9 +99,9 @@ int main(int argc, char *argv[])
 
     // ----------------------Reduce_Noise--------------------------------------
     edge_surface = reduce_noise(edge_surface);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/noise.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/noise.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -110,9 +109,9 @@ int main(int argc, char *argv[])
 
     // ----------------------Blur----------------------------------------------
     edge_surface = blur(edge_surface);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/blur.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/blur.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -121,9 +120,9 @@ int main(int argc, char *argv[])
     // ----------------------Adaptive_Threshold--------------------------------
     int noise_level = noise(edge_surface, edge_surface->w,edge_surface->h);
     adaptive_threshold(edge_surface, noise_level > 300 ? 0.5 : 0.15,32);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/threshold.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/threshold.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -131,22 +130,22 @@ int main(int argc, char *argv[])
 
     // ----------------------Sobel---------------------------------------------
     edge_surface = Sobel(edge_surface, 1);
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/sobel.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/sobel.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
         screen_surface = display_image(edge_surface);
-    bin_surface = load_image("output//treatment/threshold.png");
+    bin_surface = load_image("output/treatment/threshold.png");
 
     // ----------------------Hough_Transform_Rotate----------------------------
     edge_surface = hough_transform_rotate(edge_surface,&bin_surface);
     // hough_transform_rotate save lines.png image with lines
 
-    out.img = copy_surface(edge_surface);
-    out.file = "output/treatment/rotate.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(edge_surface);
+    out[th].file = "output/treatment/rotate.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -165,9 +164,9 @@ int main(int argc, char *argv[])
     edge_surface = scale(edge_surface, result.min_x, result.min_y, result.max_x,result.max_y);
     bin_surface = scale(bin_surface, result.min_x, result.min_y, result.max_x,result.max_y);
 
-    out.img = copy_surface(bin_surface);
-    out.file = "output/treatment/scale.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(bin_surface);
+    out[th].file = "output/treatment/scale.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
 
     if(dev_mod)
@@ -179,9 +178,9 @@ int main(int argc, char *argv[])
     // ----------------------Correct_Perspective-------------------------------
     bin_surface = correct_perspective(edge_surface, bin_surface);
 
-    out.img = copy_surface(bin_surface);
-    out.file = "output/treatment/perspective.png";
-    pthread_create(&threads[th], NULL, saveThead, &out);
+    out[th].img = copy_surface(bin_surface);
+    out[th].file = "output/treatment/perspective.png";
+    pthread_create(&threads[th], NULL, saveThead, &out[th]);
     th++;
     if(dev_mod)
     {
