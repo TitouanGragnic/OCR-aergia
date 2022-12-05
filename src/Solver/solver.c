d19263e1c8c19598de6bc501dbf69d620228d3f1
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-12-05 10:11:23 +0100

    adapt Solver for hexa grid

--- src/Solver/solver.c
+++ src/Solver/solver.c
@@ -1,52 +1,56 @@
-int valid(int matrix[][9], int row, int column, int guess) {
-     /*
-       Check if guess is valid on the matrix.
-     */
-     int corner_x = row / 3 * 3;
-     int corner_y = column / 3 * 3;
-     for (int x = 0; x < 9; ++x)
-     {
-	  if (matrix[row][x] == guess)
-	       return 0;
-	  if (matrix[x][column] == guess)
-	       return 0;
-	  if (matrix[corner_x + (x % 3)][corner_y + (x / 3)] == guess)
-	       return 0;
-     }
-     return 1;
+#include "solver.h"
+
+int valid(int** matrix, int row, int column, int guess, int width) {
+    /*
+      Check if guess is valid on the matrix.
+    */
+    int square = sqrt(width);
+    int corner_x = row / square * square;
+    int corner_y = column / square * square ;
+    for (int x = 0; x < width; ++x)
+    {
+        if (matrix[row][x] == guess)
+            return 0;
+        if (matrix[x][column] == guess)
+            return 0;
+        if (matrix[corner_x + (x % square)][corner_y + (x / square)] == guess)
+            return 0;
+    }
+    return 1;
 }
 
-int find_empty_cell(int matrix[][9], int *row, int *column) {
-     /*
-       Find first empty cell.
-     */
-     for (int x = 0; x < 9; x++)
-	  for (int y = 0; y < 9; y++)
-	       if (!matrix[x][y])
-	       {
-		    *row = x;
-		    *column = y;
-		    return 1;
-	       }
-     return 0;
+int find_empty_cell(int** matrix, int *row, int *column, int width)
+{
+    /*
+      Find first empty cell.
+    */
+    for (int x = 0; x < width; x++)
+        for (int y = 0; y < width; y++)
+            if (!matrix[x][y])
+            {
+                *row = x;
+                *column = y;
+                return 1;
+            }
+    return 0;
 }
 
-int solve(int matrix[][9]) {
+int solve(int** matrix, int width) {
     /*
       Solve the Sudoku with backtracking.
-     */
+    */
     int row;
     int column;
 
-    if(!find_empty_cell(matrix, &row, &column))
-	 return 1;
+    if(!find_empty_cell(matrix, &row, &column, width))
+        return 1;
 
-    for (int guess = 1; guess < 10; guess++)
-	if (valid(matrix, row, column, guess))
+    for (int guess = 1; guess <= width; guess++)
+	if (valid(matrix, row, column, guess, width))
 	{
 	    matrix[row][column] = guess;
-	    if(solve(matrix))
-		 return 1;
+	    if(solve(matrix, width))
+                return 1;
 	    matrix[row][column] = 0;
 	}
     return 0;
--- src/Solver/work_on_file.c
+++ src/Solver/work_on_file.c
@@ -1,106 +1,122 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include <math.h>
 void read_file(char file[], char input[]){
-     /*
-       Read file and put the content on input array.
-     */
+    /*
+      Read file and put the content on input array.
+    */
 
-     FILE *fptr;
-     int bufferLength = 255;
-     char buffer[bufferLength];
+    FILE *fptr;
+    int bufferLength = 1024;
+    char buffer[bufferLength];
 
-     if ((fptr = fopen(file,"r")) == NULL)
-     {
-	  printf("Error! opening file");
-	  exit(1);
-     }
+    if ((fptr = fopen(file,"r")) == NULL)
+    {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-     int i = 0 ;
-     while(fgets(buffer, bufferLength, fptr))
-     {
-	  int j=0;
-	  while (buffer[j] != 0)
-	  {
-	       input[i] = buffer[j];
-	       j++;
-	       i++;
-	  }
-     }
-     fclose(fptr);
+    int i = 0 ;
+    while(fgets(buffer, bufferLength, fptr))
+    {
+        int j=0;
+        while (buffer[j] != 0)
+        {
+            input[i] = buffer[j];
+            j++;
+            i++;
+        }
+    }
+    fclose(fptr);
 }
 
 
 void write_file(char name[],char result[])
 {
-     /*
-       Write a file with content of the string solver result.
-     */
-     char file[strlen(name)+7];
-     strcpy(file,name);
-     strcat(file,".result");
+    /*
+      Write a file with content of the string solver result.
+    */
+    char* file = malloc((strlen(name)+strlen(".result")+1)*sizeof(char));
+    strcpy(file,name);
+    strcat(file,".result");
 
-     FILE *fptr;
-     fptr = fopen(file,"w");
+    FILE *fptr;
+    fptr = fopen(file,"w");
 
-     fprintf(fptr,"%s",result);
+    fprintf(fptr,"%s",result);
+    free(file);
 }
 
-void decode(char input[], int tab[][9])
+void decode(char input[], int** tab,int width)
 {
-     /*
-       Decode string format to int matrix.
-     */
-     int i = 0;
-     int j = 0;
-
-     while(i<81)
-     {
-	  if(input[j] == '.')
-	  {
-	       tab[i/9][i%9] = 0;
-	       i++;
-	  }
-	  else if (input[j]>='0' && input[j]<= '9')
-	  {
-	       tab[i/9][i%9] = input[j]-'0';
-	       i++;
-	  }
-	  j++;
-     }
+    /*
+      Decode string format to int matrix.
+    */
+    int i = 0;
+    int j = 0;
+    while(i<width*width)
+    {
+        if(input[j] == '.')
+        {
+            tab[i/width][i%width] = 0;
+            i++;
+        }
+        else if (input[j]=='0')
+        {
+            tab[i/width][i%width] = 16;
+            i++;
+        }
+        else if (input[j]>'0' && input[j]<= '9')
+        {
+            tab[i/width][i%width] = input[j]-'0';
+            i++;
+        }
+        else if (input[j]>='A' && input[j]<='F')
+        {
+            tab[i/width][i%width] = input[j]-'A'+10;
+            i++;
+        }
+        j++;
+    }
 }
 
-void toString(int tab[9][9],char result[]){
-     /*
-       Convert solver result to string format.
-     */
-     int i = 0;
-     int j = 0;
-     int k = 0;
+void toString(int** tab,char result[], int width){
+    /*
+      Convert solver result to string format.
+    */
+    int i = 0;
+    int j = 0;
+    int k = 0;
 
-     while(i<9)
-     {
-	  j=0;
-	  while(j<9)
-	  {
-	       result[k] = '0'+tab[i][j];
-	       k++;
-	       if((j+1)%3 == 0)
-	       {
-		    result[k] = ' ';
-		    k++;
-	       }
-	       j++;
-	  }
-	  if((i+1)%3 == 0)
-	  {
-	       result[k] = '\n';
-	       k++;
-	  }
-	  result[k] = '\n';
-	  k++;
-	  i++;
-     }
-     result[k] = '\0';
+    int square = sqrt(width);
+    while(i<width)
+    {
+        j=0;
+        while(j<width)
+        {
+            if(tab[i][j] == 16)
+                result[k] = '0';
+            if(tab[i][j]<=9)
+                result[k] = '0'+tab[i][j];
+            else
+                result[k] = 'A'+tab[i][j]-10;
+            k++;
+            if((j+1)%square == 0)
+            {
+                result[k] = ' ';
+                k++;
+            }
+            j++;
+        }
+        if((i+1)%square == 0)
+        {
+            result[k] = '\n';
+            k++;
+        }
+        result[k] = '\n';
+        k++;
+        i++;
+    }
+    result[k] = '\0';
 }
