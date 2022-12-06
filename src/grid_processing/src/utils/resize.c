013c3033ae454b9c4cc440a8648e23dd9b2bc210
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2022-12-06 21:25:49 +0100

    fix slot slicing in grid processing

--- src/grid_processing/src/utils/resize.c
+++ src/grid_processing/src/utils/resize.c
@@ -81,3 +81,41 @@ SDL_Surface* resize_slot(SDL_Surface* start_surface)
     SDL_FreeSurface(start_surface);
     return resize_surface ;
 }
+
+SDL_Surface* resize_slot2(SDL_Surface* start_surface, int x, int y1, int y2)
+{
+    SDL_Surface* resize_surface;
+    //int w = start_surface->w;
+    //int h = start_surface->h;
+    double s;
+    Uint8 r,g,b;
+    double w = y2-y1;
+
+    int x1 = x - w/2;
+    if(x1<0)
+        x1 = 0;
+    //int x2 = x + w/2;
+    s = w/16.0;
+    Uint32 newPixel;
+    resize_surface = SDL_CreateRGBSurface(0,16,16,32,0,0,0,0);
+
+    for(int i = 0; i< 16; i++)
+        for(int j = 0; j< 16; j++)
+        {
+            if(i == 0 || j ==0 || i == 15 || j == 15)
+                put_pixel_value(resize_surface,i,j,255);
+            else
+            {
+                newPixel =get_average_color(start_surface,x1+i*s,y1+j*s,s,start_surface->format);
+                SDL_GetRGB(newPixel,start_surface->format,&r,&g,&b);
+                if(0.33*r+0.33*g+0.33*b < 150)
+                    newPixel = SDL_MapRGB(resize_surface->format, 0, 0, 0);
+                else
+                    newPixel = SDL_MapRGB(resize_surface->format, 255, 255, 255);
+                put_pixel(resize_surface,i,j,newPixel);
+            }
+        }
+
+    SDL_FreeSurface(start_surface);
+    return resize_surface ;
+}
